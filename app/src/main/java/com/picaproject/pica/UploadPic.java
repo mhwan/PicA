31c6c3ec3a570dbe515b16742d2b3b9b1de16550
박희수 <ezreal12@naver.com>
2020-01-14 21:36:04 +0900

    사진 업로드 레이아웃 완성

---
+++ app/src/main/java/com/picaproject/pica/UploadPic.java
@@ -0,0 +1,16 @@
+package com.picaproject.pica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadPic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_pic);
+    }
+
+
+
+}
