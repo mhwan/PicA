3a1c328d60c9bca35f7655a40227ff35d8f854a6
박희수 <ezreal12@naver.com>
2020-01-15 16:05:19 +0900

    사진 등록 임시구현 / 저장소 권한 묻기

--- app/src/main/java/com/picaproject/pica/Activity/UploadAlbumActivity.java
+++ app/src/main/java/com/picaproject/pica/Activity/UploadAlbumActivity.java
@@ -1,6 +1,10 @@
 package com.picaproject.pica.Activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +12,7 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.picaproject.pica.CustomView.AlbumUploadPicAdapter;
 import com.picaproject.pica.IntentProtocol;
@@ -22,6 +27,11 @@ public class UploadAlbumActivity extends AppCompatActivity {
     private ArrayList<UploadPicData> dataList;
     private AlbumUploadPicAdapter picAdapter ;
 
+
+    String[] permission_list = {
+            Manifest.permission.WRITE_CONTACTS,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     /*
       dataList.add(data);
@@ -38,6 +48,8 @@ public class UploadAlbumActivity extends AppCompatActivity {
 
         updaloadPicListView = (RecyclerView) findViewById(R.id.updaloadPicListView);
         LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
+        mLinearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+
         updaloadPicListView.setLayoutManager(mLinearLayoutManager);
         dataList = new ArrayList<UploadPicData>();
         picAdapter = new AlbumUploadPicAdapter(this,dataList);
@@ -46,28 +58,99 @@ public class UploadAlbumActivity extends AppCompatActivity {
                 mLinearLayoutManager.getOrientation());
         updaloadPicListView.addItemDecoration(dividerItemDecoration);
 
-        picAdapter.addEOFPicData();
+        addEOFPicData();
         picAdapter.notifyDataSetChanged();
-
+        checkPermission();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-
+        if(data == null){
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
         // 사진 추가 모드일경우
-        if(requestCode == IntentProtocol.ADD_PIC_MODE && resultCode == RESULT_OK && data != null && data.getData() != null){
+        if(requestCode == IntentProtocol.ADD_PIC_MODE){
             UploadPicData picData = (UploadPicData)data.getSerializableExtra("UploadPicData");
             Log.i("test_hs","UploadAlbumActivity onActivityResult : "+picData.toString());
+            addPicData(picData);
+            Log.i("test_hs","UploadAlbumActivity onActivityResult2 : "+dataList.toString());
         }
         // 사진 수정 모드일경우
         if(requestCode == IntentProtocol.UPDATE_PIC_MODE && resultCode == RESULT_OK && data != null && data.getData() != null){
             UploadPicData picData = (UploadPicData)data.getSerializableExtra("UploadPicData");
             Log.i("test_hs","UploadAlbumActivity onActivityResult : "+picData.toString());
+            Log.i("test_hs","UploadAlbumActivity onActivityResult3 : "+dataList.toString());
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    /*
+     *  Uri가 NULL이고 contents값이 "EOF" 문자열인 UploadPicData를
+     *  dataList 끝에 집어넣는다.
+     *  이 EOF 데이터는 RecyclerView에서 onBindViewHolder를 통해 View를 실제로 그려줄때
+     *  "사진 추가" 화면으로 이동하는 버튼 역할을 한다.
+     *  dataList에 새 데이터를 추가하기 전에 dataList 끝에 존재할 EOF 데이터를 반드시 삭제하고 추가해야한다.
+     *  또한 새 데이터가 추가된 후에도 끝을 표시하는 EOF가 필요하다
+     * */
+    private void addEOFPicData(){
+        UploadPicData eofData = new UploadPicData(null);
+        eofData.setContents(UploadPicData.STATE_EOF);
+        dataList.add(eofData);
+    }
+    /*
+     *  반드시 dataList에 데이터가 추가되기 전에 호출되어 EOF를 제거한 후
+     *  addEOFPicData를 호출하여 다시 EOF를 표시하는 데이터를 추가해야 한다.
+     *
+     * */
+    private void removeEOFPicData(){
+        dataList.remove(dataList.size()-1);
+    }
 
+    private void addPicData(UploadPicData data){
+        // Array 가장 끝에 있을 EOF 제거
+        removeEOFPicData();
+        dataList.add(data);
+        addEOFPicData();
+        // 리사이클 뷰 새로고침
+        picAdapter.notifyDataSetChanged();
 
+        Log.i("test_hs","UploadAlbumActivity addPicData : "+dataList.toString());
+    }
+
+    public void checkPermission(){
+        //현재 안드로이드 버전이 6.0미만이면 메서드를 종료한다.
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return;
+
+        for(String permission : permission_list){
+            //권한 허용 여부를 확인한다.
+            int chk = checkCallingOrSelfPermission(permission);
+
+            if(chk == PackageManager.PERMISSION_DENIED){
+                //권한 허용을여부를 확인하는 창을 띄운다
+                requestPermissions(permission_list,0);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==0)
+        {
+            for(int i=0; i<grantResults.length; i++)
+            {
+                //허용됬다면
+                if(grantResults[i]==PackageManager.PERMISSION_GRANTED){
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"저장소 접근 권한을 허용해주셔야 이용이 가능합니다.",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
+    }
 
 }
+
--- app/src/main/java/com/picaproject/pica/CustomView/AlbumUploadPicAdapter.java
+++ app/src/main/java/com/picaproject/pica/CustomView/AlbumUploadPicAdapter.java
@@ -27,27 +27,7 @@ public class AlbumUploadPicAdapter extends RecyclerView.Adapter<UploadPicHolder>
         this.dataList = list;
         this.context = context;
     }
-    /*
-    *  Uri가 NULL이고 contents값이 "EOF" 문자열인 UploadPicData를
-    *  dataList 끝에 집어넣는다.
-    *  이 EOF 데이터는 RecyclerView에서 onBindViewHolder를 통해 View를 실제로 그려줄때
-    *  "사진 추가" 화면으로 이동하는 버튼 역할을 한다.
-    *  dataList에 새 데이터를 추가하기 전에 dataList 끝에 존재할 EOF 데이터를 반드시 삭제하고 추가해야한다.
-    *  또한 새 데이터가 추가된 후에도 끝을 표시하는 EOF가 필요하다
-    * */
-    public void addEOFPicData(){
-        UploadPicData eofData = new UploadPicData(null);
-        eofData.setContents(UploadPicData.STATE_EOF);
-        dataList.add(eofData);
-    }
-    /*
-    *  반드시 dataList에 데이터가 추가되기 전에 호출되어 EOF를 제거한 후
-    *  addEOFPicData를 호출하여 다시 EOF를 표시하는 데이터를 추가해야 한다.
-    *
-    * */
-    public void removeEOFPicData(){
-        dataList.remove(dataList.size());
-    }
+
 
     // nonNUll 어노테이션은 해당 변수에 null값이 넘어오면 예외를 발생시켜줌으로써 null을방지함
     @NonNull
@@ -86,6 +66,7 @@ public class AlbumUploadPicAdapter extends RecyclerView.Adapter<UploadPicHolder>
 
             }catch (Exception e){
                 String err = "이미지 로딩 에러 발생 " + e.toString();
+                Log.e("test_hs",err);
                 Log.e(this.getClass().getName(),err);
                 uploadPicHolder.getImageView().setImageResource(R.drawable.error_icon);
             }
