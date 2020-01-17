8fbf8cfdc2b15fccabc802eb899756e4fcd94068
박희수 <ezreal12@naver.com>
2020-01-17 16:35:20 +0900

    PermissionChecker 구현 / 모듈 설명 문서 작성

--- app/src/main/java/com/picaproject/pica/Activity/UploadAlbumActivity.java
+++ app/src/main/java/com/picaproject/pica/Activity/UploadAlbumActivity.java
@@ -1,32 +1,29 @@
 package com.picaproject.pica.Activity;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.Toast;
+        import android.Manifest;
+        import android.content.Intent;
+        import android.support.annotation.NonNull;
+        import android.support.annotation.Nullable;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.support.v7.widget.DividerItemDecoration;
+        import android.support.v7.widget.LinearLayoutManager;
+        import android.support.v7.widget.RecyclerView;
+        import android.util.Log;
+        import com.picaproject.pica.CustomView.AlbumUploadPicAdapter;
+        import com.picaproject.pica.IntentProtocol;
+        import com.picaproject.pica.Item.UploadPicData;
+        import com.picaproject.pica.R;
+        import com.picaproject.pica.Util.PermissionChecker;
 
-import com.picaproject.pica.CustomView.AlbumUploadPicAdapter;
-import com.picaproject.pica.IntentProtocol;
-import com.picaproject.pica.Item.UploadPicData;
-import com.picaproject.pica.R;
-
-import java.util.ArrayList;
+        import java.util.ArrayList;
 
 public class UploadAlbumActivity extends AppCompatActivity {
 
     private RecyclerView updaloadPicListView;
     private ArrayList<UploadPicData> dataList;
     private AlbumUploadPicAdapter picAdapter ;
-
+    private PermissionChecker pc;
 
     String[] permission_list = {
             Manifest.permission.WRITE_CONTACTS,
@@ -62,7 +59,9 @@ public class UploadAlbumActivity extends AppCompatActivity {
 
         addEOFPicData();
         picAdapter.notifyDataSetChanged();
-        checkPermission();
+        //필수 2줄
+        pc = new PermissionChecker(permission_list,this);
+        pc.checkPermission();
     }
 
     @Override
@@ -129,38 +128,12 @@ public class UploadAlbumActivity extends AppCompatActivity {
         picAdapter.notifyDataSetChanged();
     }
 
-    public void checkPermission(){
-        //현재 안드로이드 버전이 6.0미만이면 메서드를 종료한다.
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
-            return;
-
-        for(String permission : permission_list){
-            //권한 허용 여부를 확인한다.
-            int chk = checkCallingOrSelfPermission(permission);
-
-            if(chk == PackageManager.PERMISSION_DENIED){
-                //권한 허용을여부를 확인하는 창을 띄운다
-                requestPermissions(permission_list,0);
-            }
-        }
-    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode==0)
-        {
-            for(int i=0; i<grantResults.length; i++)
-            {
-                //허용됬다면
-                if(grantResults[i]==PackageManager.PERMISSION_GRANTED){
-                }
-                else {
-                    Toast.makeText(getApplicationContext(),"저장소 접근 권한을 허용해주셔야 이용이 가능합니다.",Toast.LENGTH_LONG).show();
-                    finish();
-                }
-            }
-        }
+        //필수
+        pc.requestPermissionsResult(requestCode,grantResults);
     }
 
 }
---
+++ app/src/main/java/com/picaproject/pica/Util/PermissionChecker.java
@@ -0,0 +1,54 @@
+package com.picaproject.pica.Util;
+
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+
+public class PermissionChecker {
+    // 받고싶은 권한을 배열로 전달받음
+    private String[] permission_list;
+    // 권한을 요구하고싶은 액티비티
+    private AppCompatActivity activity;
+
+    public PermissionChecker(String[] permission_list, AppCompatActivity activity) {
+        this.permission_list = permission_list;
+        this.activity = activity;
+    }
+
+    // OnCreate에서 API 버전과 상관없이 무조건 호출필요
+    // API 버전에 따른 체크는 함수내부에서 진행
+    public void checkPermission(){
+        //현재 안드로이드 버전이 6.0미만이면 메서드를 종료한다.
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return;
+
+        for(String permission : permission_list){
+            //권한 허용 여부를 확인한다.
+            int chk = activity.checkCallingOrSelfPermission(permission);
+
+            if(chk == PackageManager.PERMISSION_DENIED){
+                //권한 허용을여부를 확인하는 창을 띄운다
+                activity.requestPermissions(permission_list,0);
+            }
+        }
+    }
+    // 각 AppCompatActivity의 onRequestPermissionsResult 메소드를 재정의 한 후 이 PermissionChecker의 requestPermissionsResult 호출
+    public void requestPermissionsResult(int requestCode,int[] grantResults){
+        if(requestCode==0)
+        {
+            for(int i=0; i<grantResults.length; i++)
+            {
+                //허용됬다면
+                if(grantResults[i]==PackageManager.PERMISSION_GRANTED){
+                }
+                else {
+                    Toast.makeText(activity.getApplicationContext(),"저장소 접근 권한을 허용해주셔야 이용이 가능합니다.",Toast.LENGTH_LONG).show();
+                    activity.finish();
+                }
+            }
+        }
+    }
+
+}
