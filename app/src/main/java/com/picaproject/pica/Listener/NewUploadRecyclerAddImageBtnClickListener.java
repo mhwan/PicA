ddbf7e9d14b8615961c02a91d4dd562d0876c93e
박희수 <ezreal12@naver.com>
2020-01-29 02:42:47 +0900

    리사이클러 뷰의 이미지 선택시 플래그먼트 화면 위치도 같이바뀜 / 사진 추가 화면 + 버튼 기능구현

---
+++ app/src/main/java/com/picaproject/pica/Listener/NewUploadRecyclerAddImageBtnClickListener.java
@@ -0,0 +1,28 @@
+package com.picaproject.pica.Listener;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.picaproject.pica.IntentProtocol;
+
+// 사진추가 화면에서 + (사진추가) 버튼을 눌렀을때 동작
+public class NewUploadRecyclerAddImageBtnClickListener implements View.OnClickListener {
+
+    private AppCompatActivity activity;
+
+    public NewUploadRecyclerAddImageBtnClickListener(AppCompatActivity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public void onClick(View view) {
+        // 외부 갤러리 앱 열기
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        //startActivityForResult(intent, IntentProtocol.GET_GALLERY_IMAGE);
+        activity.startActivityForResult(Intent.createChooser(intent,"Select Picture"), IntentProtocol.GET_GALLERY_IMAGE);
+    }
+}
