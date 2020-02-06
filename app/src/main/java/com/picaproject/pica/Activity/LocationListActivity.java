1d6aa38acbd01dd846ac728a90473fb4251ce65d
박희수 <ezreal12@naver.com>
2020-02-06 20:17:10 +0900

    주변위치 표시 임시구현(실행만)

--- app/src/main/java/com/picaproject/pica/Activity/LocationListActivity.java
+++ app/src/main/java/com/picaproject/pica/Activity/LocationListActivity.java
@@ -19,6 +19,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
@@ -45,12 +46,20 @@ import com.picaproject.pica.R;
 import com.picaproject.pica.Util.PermissionChecker;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 
+import noman.googleplaces.NRPlaces;
+import noman.googleplaces.Place;
+import noman.googleplaces.PlaceType;
+import noman.googleplaces.PlacesException;
+import noman.googleplaces.PlacesListener;
+
 public class LocationListActivity extends AppCompatActivity
         implements OnMapReadyCallback,
-        ActivityCompat.OnRequestPermissionsResultCallback {
+        ActivityCompat.OnRequestPermissionsResultCallback, PlacesListener {
 
 
     private BottomSheetBehavior bottomSheetBehavior;
@@ -98,7 +107,7 @@ public class LocationListActivity extends AppCompatActivity
 
         setContentView(R.layout.activity_location_list);
 
-
+        previous_marker = new ArrayList<Marker>();
         FrameLayout bottomsheet = findViewById(R.id.bottom_sheet);
         bottomSheetBehavior = BottomSheetBehavior.from(bottomsheet);
 
@@ -124,6 +133,9 @@ public class LocationListActivity extends AppCompatActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
     }
 
     @Override
@@ -195,8 +207,12 @@ public class LocationListActivity extends AppCompatActivity
             public void onMapClick(LatLng latLng) {
 
                 Log.d( TAG, "onMapClick :");
+                //TODO
+                showPlaceInformation(currentPosition);
             }
         });
+
+
     }
 
     LocationCallback locationCallback = new LocationCallback() {
@@ -534,6 +550,75 @@ public class LocationListActivity extends AppCompatActivity
 
                 break;
         }
+    }
+
+
+    List<Marker> previous_marker = null;
+
+    // PlacesListener 오버라이딩
+    @Override
+    public void onPlacesFailure(PlacesException e) {
+
+    }
+
+    @Override
+    public void onPlacesStart() {
+
+    }
+
+    @Override
+    public void onPlacesSuccess(final List<Place> places) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                for (noman.googleplaces.Place place : places) {
+
+                    LatLng latLng
+                            = new LatLng(place.getLatitude()
+                            , place.getLongitude());
+
+                    String markerSnippet = getCurrentAddress(latLng);
+
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(latLng);
+                    markerOptions.title(place.getName());
+                    markerOptions.snippet(markerSnippet);
+                    Marker item = mMap.addMarker(markerOptions);
+                    previous_marker.add(item);
+
+                }
+
+                //중복 마커 제거
+                HashSet<Marker> hashSet = new HashSet<Marker>();
+                hashSet.addAll(previous_marker);
+                previous_marker.clear();
+                previous_marker.addAll(hashSet);
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onPlacesFinished() {
+
+    }
+
+    public void showPlaceInformation(LatLng location)
+    {
+        mMap.clear();//지도 클리어
+
+        if (previous_marker != null)
+            previous_marker.clear();//지역정보 마커 클리어
+
+        new NRPlaces.Builder()
+                .listener(LocationListActivity.this)
+                .key("AIzaSyCGtSKl81RRcPu9xNFYXU3N4zBkwAFXSN8")
+                .latlng(location.latitude, location.longitude)//현재 위치
+                .radius(500) //500 미터 내에서 검색
+                .type(PlaceType.RESTAURANT) //음식점
+                .build()
+                .execute();
     }
 
 }
